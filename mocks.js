a0873c80278ceb0638939ef024519cdab244c6cc
Wojciech Gomola <wgomola@avaya.com>
2016-08-19 20:59:39 +0200

    Mock draw

--- mocks.js
+++ mocks.js
@@ -1,14 +1,211 @@
-
 var mocks = {
     getter: {
         users: [{
             name: 'test test',
-            email: 'test@test.pl'
+            emaill: 'test@test.pl'
         }],
         ping: {
             date: '2016-08-29T17:52:52.298Z'
-        }
+        },
+        draw: [{
+            date: '2016-08-12T12:15:52.298Z',
+            winners: [{
+                "winner": {
+                    "name": "Scott Oliver",
+                    "email": "soliver0@mapquest.com"
+                },
+                "card": {
+                    "name": "Card 01",
+                    "type": "Parking bliży"
+                }
+            }, {
+                "winner": {
+                    "name": "Charles Campbell",
+                    "email": "ccampbell1@economist.com"
+                },
+                "card": {
+                    "name": "Card 02",
+                    "type": "Salvador Dali"
+                }
+            }, {
+                "winner": {
+                    "name": "Douglas Carr",
+                    "email": "dcarr2@ustream.tv"
+                },
+                "card": {
+                    "name": "Card 03",
+                    "type": "Parking bliży"
+                }
+            }, {
+                "winner": {
+                    "name": "Angela Fox",
+                    "email": "afox3@list-manage.com"
+                },
+                "card": {
+                    "name": "Card 04",
+                    "type": "Salvador Dali"
+                }
+            }],
+            users: [{
+                "name": "Scott Oliver",
+                "email": "soliver0@mapquest.com"
+            }, {
+                "name": "Charles Campbell",
+                "email": "ccampbell1@economist.com"
+            }, {
+                "name": "Douglas Carr",
+                "email": "dcarr2@ustream.tv"
+            }, {
+                "name": "Angela Fox",
+                "email": "afox3@list-manage.com"
+            }, {
+                "name": "Angela Collins",
+                "email": "acollins4@hc360.com"
+            }, {
+                "name": "Eric Bennett",
+                "email": "ebennett5@reverbnation.com"
+            }, {
+                "name": "Evelyn Wilson",
+                "email": "ewilson6@cisco.com"
+            }, {
+                "name": "George Lewis",
+                "email": "glewis7@npr.org"
+            }, {
+                "name": "Gloria Peters",
+                "email": "gpeters8@so-net.ne.jp"
+            }, {
+                "name": "Chris Lane",
+                "email": "clane9@360.cn"
+            }],
+            specialCases: []
+        }, {
+            date: '2016-07-29T12:15:52.298Z',
+            winners: [ {
+                "winner": {
+                    "name": "Douglas Carr",
+                    "email": "dcarr2@ustream.tv"
+                },
+                "card": {
+                    "name": "Card 01",
+                    "type": "Parking bliży"
+                }
+            }, {
+                "winner": {
+                    "name": "Angela Fox",
+                    "email": "afox3@list-manage.com"
+                },
+                "card": {
+                    "name": "Card 02",
+                    "type": "Salvador Dali"
+                }
+            }, {
+                "winner": {
+                    "name": "Angela Collins",
+                    "email": "acollins4@hc360.com"
+                },
+                "card": {
+                    "name": "Card 03",
+                    "type": "Parking bliży"
+                }
+            }, {
+                "winner": {
+                    "name": "Eric Bennett",
+                    "email": "ebennett5@reverbnation.com"
+                },
+                "card": {
+                    "name": "Card 04",
+                    "type": "Salvador Dali"
+                }
+            }],
+            users: [{
+                "name": "Scott Oliver",
+                "email": "soliver0@mapquest.com"
+            }, {
+                "name": "Charles Campbell",
+                "email": "ccampbell1@economist.com"
+            }, {
+                "name": "Douglas Carr",
+                "email": "dcarr2@ustream.tv"
+            }, {
+                "name": "Angela Fox",
+                "email": "afox3@list-manage.com"
+            }, {
+                "name": "Angela Collins",
+                "email": "acollins4@hc360.com"
+            }, {
+                "name": "Eric Bennett",
+                "email": "ebennett5@reverbnation.com"
+            }, {
+                "name": "Evelyn Wilson",
+                "email": "ewilson6@cisco.com"
+            }, {
+                "name": "George Lewis",
+                "email": "glewis7@npr.org"
+            }],
+            specialCases: []
+        }, {
+            date: '2016-07-15T12:15:52.298Z',
+            winners: [{
+                "winner": {
+                    "name": "Angela Fox",
+                    "email": "afox3@list-manage.com"
+                },
+                "card": {
+                    "name": "Card 02",
+                    "type": "Salvador Dali"
+                }
+            }, {
+                "winner": {
+                    "name": "Angela Collins",
+                    "email": "acollins4@hc360.com"
+                },
+                "card": {
+                    "name": "Card 03",
+                    "type": "Parking bliży"
+                }
+            }, {
+                "winner": {
+                    "name": "Eric Bennett",
+                    "email": "ebennett5@reverbnation.com"
+                },
+                "card": {
+                    "name": "Card 04",
+                    "type": "Salvador Dali"
+                }
+            }, {
+                "winner": {
+                    "name": "Evelyn Wilson",
+                    "email": "ewilson6@cisco.com"
+                },
+                "card": {
+                    "name": "Card 01",
+                    "type": "Parking bliży"
+                }
+            }],
+            users: [{
+                "name": "Angela Fox",
+                "email": "afox3@list-manage.com"
+            }, {
+                "name": "Angela Collins",
+                "email": "acollins4@hc360.com"
+            }, {
+                "name": "Eric Bennett",
+                "email": "ebennett5@reverbnation.com"
+            }, {
+                "name": "Evelyn Wilson",
+                "email": "ewilson6@cisco.com"
+            }, {
+                "name": "George Lewis",
+                "email": "glewis7@npr.org"
+            }, {
+                "name": "Gloria Peters",
+                "email": "gpeters8@so-net.ne.jp"
+            }, {
+                "name": "Chris Lane",
+                "email": "clane9@360.cn"
+            }],
+            specialCases: []
+        }]
     }
 };
-
 module.exports = mocks;
