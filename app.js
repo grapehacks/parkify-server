fdc30ea71bc55c9d1cc52fc152f39d3b692dea11
Pawel Mazur <pamz@grapeup.com>
2016-08-20 04:46:16 +0200

    asd

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     if ('OPTIONS' == req.method) {
-        res.send(200);
+        res.status(200);
     } else {
         next();
     }
