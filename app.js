c2677a3612bbd26c7fad46a43376494cdda95489
Pawel Mazur <pamz@grapeup.com>
2016-08-20 05:38:13 +0200

    xczcx

--- app.js
+++ app.js
@@ -85,35 +85,49 @@ app.use(function (err, req, res, next) {
 });
 
 
-if (true) {
+if (false) {
     var User = mongoose.model('User', require('./models/Users.js'));
     User.find({}).remove(function() {
-       User.create(
-           {
-               name: 'Test A',
-               email: 'a',
-               password: 'a',
-               type: 1
-           },
-           {
-               name: 'Test User1',
-               email: 'test1@test.com',
-               password: 'pass',
-               type: 0
-           },
-           {
-               name: 'Test User2',
-               email: 'test2@test.com',
-               password: 'pass',
-               type: 0
-           },
-           {
-               name: 'Test User3',
-               email: 'test3@test.com',
-               password: 'pass',
-               type: 0
-           }
-       );
+        User.create(
+            {
+                name: 'Test A',
+                email: 'a',
+                password: 'a',
+                type: 1
+            },
+            {
+                name: 'Test User1',
+                email: 'test1@test.com',
+                password: 'pass',
+                type: 0
+            },
+            {
+                name: 'Test User2',
+                email: 'test2@test.com',
+                password: 'pass',
+                type: 0
+            },
+            {
+                name: 'Test User3',
+                email: 'test3@test.com',
+                password: 'pass',
+                type: 0
+            }
+        );
+    });
+
+    var Card = mongoose.model('Card', require('./models/Cards.js'));
+    Card.find({}).remove(function() {
+        Card.create(
+            {
+                name: 'Card 2',
+                type: 'c2'
+            },
+            {
+                name: 'Card 1',
+                type: 'c1'
+            }
+        );
     });
 }
 
--- services/Drawer.js
+++ services/Drawer.js
@@ -4,14 +4,16 @@ var User = mongoose.model('User', require('../models/Users.js'));
 var Card = mongoose.model('Card', require('./../models/Cards.js'));
 var DrawDate = mongoose.model('DrawDate', require('../models/DrawDate.js'));
 
-var draw = function(users, cards) {
+var draw = function (users, cards) {
     var drawNext = function drawNext(users, cards, results) {
         console.log("users ", users.length, " cards ", cards.length, " results ", results.length);
 
         if (cards.length === 0 || users.length == 0) {
             return results;
         }
-        var sumWeight = users.reduce(function(prev, curr) { return prev + curr.weight}, 0.0);
+        var sumWeight = users.reduce(function (prev, curr) {
+            return prev + curr.weight
+        }, 0.0);
         var drawnValue = Math.random() * sumWeight;
 
         var drawnUserData = users.reduce(function (prev, curr, index) {
@@ -28,7 +30,7 @@ var draw = function(users, cards) {
                 userIndex: userIndex,
                 subSum: subSum
             }
-        }, { userIndex: undefined, subSum:0.0 });
+        }, {userIndex: undefined, subSum: 0.0});
 
         if (drawnUserData.userIndex === undefined) {
             throw new Error("Unable to draw a user");
@@ -43,22 +45,22 @@ var draw = function(users, cards) {
         cards.splice(cardIndex, 1);
 
         results.push({
-            "winner" : user,
-            "card" : card
+            "winner": user,
+            "card": card
         });
         return drawNext(users, cards, results);
     };
 
     return drawNext(users, cards, []);
-}
+};
 
-var Drawer = function (){
+var Drawer = function () {
 
-    this.setDrawDate = function(drawDate){
+    this.setDrawDate = function (drawDate) {
         // TODO: Validation
         var prommise = new Promise(function (resolve, reject) {
             DrawDate.remove({}, function (err) {
-                if (err){
+                if (err) {
                     reject();
                     return;
                 }
@@ -78,30 +80,29 @@ var Drawer = function (){
 
     this.draw = function () {
         var promise = new Promise(function (resolve, reject) {
-            User
-                .find(
-                    {"removed" : false},
-                    function (err, items) {
-                        var users = items.filter(function(item){
-                            return item.participate !== 0;
-                        });
-                        // TODO: Find better way to assign weight
-                        users.forEach(function (item) {
-                            item.weight = 1.0;
-                        });
-
-                        Card.find(
-                            {
-                                "removed": false,
-                                "active": true
-                            },
-                            function(err2, cards){
-                                console.log("Cards ", cards);
-                                var drawResult = draw(users, cards);
-                                resolve(drawResult);
-                            }
-                        );
+            User.find(
+                {"removed": false},
+                function (err, items) {
+                    var users = items.filter(function (item) {
+                        return item.participate !== 0;
                     });
+                    // TODO: Find better way to assign weight
+                    users.forEach(function (item) {
+                        item.weight = 1.0;
+                    });
+
+                    Card.find(
+                        {
+                            "removed": false,
+                            "active": true
+                        },
+                        function (err2, cards) {
+                            console.log("Cards ", cards);
+                            var drawResult = draw(users, cards);
+                            resolve(drawResult);
+                        }
+                    );
+                });
         });
         return promise;
     }
